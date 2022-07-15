c781e225230dc0ecc35b2d54c05d74ec98494bda
Muhammad Awais <awaistopper20@gmail.com>
2022-07-15 11:12:56 +0500

    Added route guade to PageThreadShow.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
 import PageNotFound from '@/components/PageNotFound.vue'
+import sourceData from '@/data.json'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -13,7 +14,26 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exists
+      const threads = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      )
+      // if exsits, go to thread
+      if (threads) {
+        return next()
+      } else {
+        // if not, redirect to 404
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve query and hash
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
