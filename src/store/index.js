79045fd2a55c36cc3c711d05359248611ba948fe
Muhammad Awais <awaistopper20@gmail.com>
2022-07-17 17:46:11 +0500

    Added UserProfileCard component in ProfileView.

--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,29 @@ export default createStore({
     }
   },
   getters: {
-    authUser: (state) => state.users.find((user) => user.id === state.authId)
+    authUser: (state) => {
+      const user = state.users.find((user) => user.id === state.authId)
+      if (!user) return null
+      return {
+        ...user,
+        // authUser.posts
+        get posts () {
+          return state.posts.filter((post) => post.userId === user.id)
+        },
+        // authUser.postsCount
+        get postsCount () {
+          return this.posts.length
+        },
+        // authUser.threads
+        get threads () {
+          return state.threads.filter((thread) => thread.userId === user.id)
+        },
+        // authUser.threadsCount
+        get threadsCount () {
+          return this.threads.length
+        }
+      }
+    }
   },
   mutations: {
     setPost (state, { post }) {
