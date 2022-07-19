3e68c420d7a3376715543b837abd3513e6eb5753
Muhammad Awais <awaistopper20@gmail.com>
2022-07-19 17:54:23 +0500

    Divided store into modules

---
+++ src/store/actions.js
@@ -0,0 +1,111 @@
+import { findById } from '@/helpers'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+export default {
+  createPost ({ commit, state }, post) {
+    post.id = 'aaaa' + Math.random()
+    post.userId = state.authId
+    post.publishedAt = Math.floor(Date.now() / 1000)
+    commit('setItem', { resource: 'posts', item: post }) // set the post
+    commit('appendPostToThread', {
+      childId: post.id,
+      parentId: post.threadId
+    }) // append the post to the thread
+    commit('appendContributorToThread', {
+      parentId: post.threadId,
+      childId: state.authUser
+    }) // append contributor to the thread
+  },
+  updateUser ({ commit }, user) {
+    commit('setItem', { resources: 'users', item: user })
+  },
+  async createThread ({ commit, state, dispatch }, { title, text, forumId }) {
+    const id = 'aaaa' + Math.random()
+    const userId = state.authId
+    const publishedAt = Math.floor(Date.now() / 1000)
+    const thread = { forumId, title, publishedAt, userId, id }
+    commit('setItem', { resource: 'threads', item: thread })
+    commit('appendThreadToForum', { parentId: forumId, childId: id })
+    commit('appendThreadToUser', { parentId: userId, childId: id })
+    dispatch('createPost', { text, threadId: id })
+    return findById(state.threads, id) // return the thread
+  },
+  async updateThread ({ commit, state }, { title, text, id }) {
+    const thread = findById(state.threads, id)
+    const post = findById(state.posts, thread.posts[0].id)
+    const newThread = { ...thread, title }
+    const newPost = { ...post, text }
+    commit('setItem', { resource: 'threads', item: newThread })
+    commit('setItem', { resource: 'posts', item: newPost })
+    return newThread
+  },
+  // ---------------------------------------
+  // Fetch Single Resource
+  // ---------------------------------------
+  fetchCategory: ({ dispatch }, { id }) =>
+    dispatch('fetchItem', { emoji: '🏷', resource: 'categories', id }),
+  fetchForum: ({ dispatch }, { id }) =>
+    dispatch('fetchItem', { emoji: '🏁', resource: 'forums', id }),
+  fetchThread: ({ dispatch }, { id }) =>
+    dispatch('fetchItem', { emoji: '📄', resource: 'threads', id }),
+  fetchPost: ({ dispatch }, { id }) =>
+    dispatch('fetchItem', { emoji: '💬', resource: 'posts', id }),
+  fetchUser: ({ dispatch }, { id }) =>
+    dispatch('fetchItem', { emoji: '🙋', resource: 'users', id }),
+  // ---------------------------------------
+  // Fetch All of a Resource
+  // ---------------------------------------
+  fetchAllCategories ({ commit }) {
+    console.log('🔥', '🏷', 'all')
+    return new Promise((resolve) => {
+      firebase
+        .firestore()
+        .collection('categories')
+        .onSnapshot((querySnapshot) => {
+          const categories = querySnapshot.docs.map((doc) => {
+            const item = { id: doc.id, ...doc.data() }
+            commit('setItem', { resource: 'categories', item })
+            return item
+          })
+          resolve(categories)
+        })
+    })
+  },
+  // ---------------------------------------
+  // Fetch Multiple Resources
+  // ---------------------------------------
+  fetchCategories: ({ dispatch }, { ids }) =>
+    dispatch('fetchItems', {
+      resource: 'categories',
+      ids,
+      emoji: '🏷'
+    }),
+  fetchForums: ({ dispatch }, { ids }) =>
+    dispatch('fetchItems', { resource: 'forums', ids, emoji: '🏁' }),
+  fetchThreads: ({ dispatch }, { ids }) =>
+    dispatch('fetchItems', { resource: 'threads', ids, emoji: '📄' }),
+  fetchPosts: ({ dispatch }, { ids }) =>
+    dispatch('fetchItems', { resource: 'posts', ids, emoji: '💬' }),
+  fetchUsers: ({ dispatch }, { ids }) =>
+    dispatch('fetchItems', { resource: 'users', ids, emoji: '🙋' }),
+  fetchItem ({ state, commit }, { id, emoji, resource }) {
+    console.log('🔥', emoji, id)
+    return new Promise((resolve) => {
+      firebase
+        .firestore()
+        .collection(resource)
+        .doc(id)
+        .onSnapshot((doc) => {
+          const item = { ...doc.data(), id: doc.id }
+          commit('setItem', { resource, item })
+          resolve(item)
+        })
+    })
+  },
+  fetchItems ({ dispatch }, { ids, resource, emoji }) {
+    return Promise.all(
+      ids.map((id) => dispatch('fetchItem', { id, resource, emoji }))
+    )
+  }
+}
