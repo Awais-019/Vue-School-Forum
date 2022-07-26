4d966369344c46832348dd5c8f38203ffd48774e
Muhammad Awais <awaistopper20@gmail.com>
2022-07-26 17:03:41 +0500

    Deploying to Netlify.

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,10 +5,10 @@ export const findById = (resources, id) => {
 
 export const upsert = (resources, resource) => {
   const index = resources?.findIndex((r) => r.id === resource.id)
-  if (resource.id && index !== -1) {
+  if (resources && resource.id && index !== -1) {
     resources[index] = resource
   } else {
-    resources.push(resource)
+    resources?.push(resource)
   }
 }
 
